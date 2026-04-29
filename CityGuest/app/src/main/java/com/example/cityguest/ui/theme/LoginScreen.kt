5dd54278e16b65c52fdc4e8da521baae4f13b034
Tedaldi Giada <giadatedaldi1@gmail.com>
2026-04-29 12:40:58 +0200

    Add message error for particular case with email or password

--- CityGuest/app/src/main/java/com/example/cityguest/ui/theme/LoginScreen.kt
+++ CityGuest/app/src/main/java/com/example/cityguest/ui/theme/LoginScreen.kt
@@ -55,6 +55,15 @@ fun LoginScreen(
 
         Spacer(modifier = Modifier.height(24.dp))
 
+        if (viewModel.errorMessage != null) {
+            Text(
+                text = viewModel.errorMessage!!,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+
         Button(
             onClick = { viewModel.onLoginClick(onLoginSuccess) },
             modifier = Modifier.fillMaxWidth().height(50.dp)
