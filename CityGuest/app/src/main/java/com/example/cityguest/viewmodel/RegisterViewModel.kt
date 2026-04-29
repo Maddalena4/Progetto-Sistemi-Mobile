32b3a3d2f0ff8fd838c57427ae245dee58b687d6
Tedaldi Giada <giadatedaldi1@gmail.com>
2026-04-29 12:33:43 +0200

    Updated Login and Register with Data

--- CityGuest/app/src/main/java/com/example/cityguest/viewmodel/RegisterViewModel.kt
+++ CityGuest/app/src/main/java/com/example/cityguest/viewmodel/RegisterViewModel.kt
@@ -4,22 +4,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cityguest.data.User
+import com.example.cityguest.data.UserRepository
+import kotlinx.coroutines.launch
 
-class RegisterViewModel: ViewModel() {
+class RegisterViewModel(private val repository: UserRepository): ViewModel() {
     // Stato dei campi di testo
     var username by mutableStateOf("")
     var email by mutableStateOf("")
     var password by mutableStateOf("")
     var confirmPassword by mutableStateOf("")
+    var errorMessage by mutableStateOf<String?>(null)
 
     // Logica di validazione basilare
     fun onRegisterClick(onSuccess: () -> Unit) {
-        if (email.contains("@") && password.length >= 6 && password == confirmPassword) {
-            // Qui in futuro chiameremo il database
-            onSuccess()
-        } else {
-            // Qui potresti gestire un messaggio di errore
-            println("Errore: controlla i dati inseriti")
+        viewModelScope.launch {
+            if (repository.isEmailRegistered(email)) {
+                errorMessage = "Questo profilo esiste già!"
+            } else {
+                // Crea il profilo
+                repository.register(User(email, username, password))
+                errorMessage = null
+                onSuccess()
+            }
         }
     }
 }
